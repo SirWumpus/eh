SirWumpus/eh

commit e4fd047c2e6f51ec344f015120410206a9baf958

CB #28 Add commands `+` and `-` to move down and up by a physical line.  Fix the behaviour of physical line commands (`cc`, `dd`, `yy`, `<<`, `>>`).

eh.c
@@ -57,7 +57,7 @@
 
 #ifndef IOCCC
 #define MATCHES		10
-#define MOTION_CMDS	32
+#define MOTION_CMDS	34
 
 static char chg = NOCHANGE;
 static int cur_row, cur_col, count, ere_dollar_only, ere_carat_only, search_wrapped, replace_all;
@@ -705,6 +705,26 @@ down(void)
 }
 
 #ifndef IOCCC
+/**
+ * Down one physical line.  The cursor moves to column 1.
+ * Physical line column is not tracked.
+ */
+void
+lnplus(void)
+{
+	here = col_or_eol(here, 0, MAX_COLS)+(here < pos(ebuf));
+}
+
+/**
+ * Up one physical line.  The cursor moves to column 1.
+ * Physical line column is not tracked.
+ */
+void
+lnminus(void)
+{
+	here = bol(bol(here)-(0 < here));
+}
+
 /*
  * Beginning of physical line.
  */
@@ -2071,6 +2091,8 @@ static struct binding cmds[] = {
 	{ CTRL_B,	pgup },
 	{ '{',		paraup },
 	{ '}',		paradown },
+	{ '+',		lnplus },
+	{ '-',		lnminus },
 
 	/* Edit */
 	{ KEY_DC,	delx },
@@ -2152,8 +2174,8 @@ getcmd(const int m)
 			SET_PREVIOUS_MARK(here);
 			(void) ungetstr("``");
 		}
-		/* Operate on line units. */
-		ch = 'j';
+		/* Operate on physical line units. */
+		ch = '+';
 		lnbegin();
 	}
 	for (j = 0; cmds[j].key not_eq -1 and ch not_eq cmds[j].key; j++) {

commit df2b8f2be0804d1d861204ae4a06123b5768311b

CB #30 Fix double character commands, eg. `dd`, `>>` et al. when the last line has no newline.  Commit e4fd047c2e introduced this issue.

eh.c
@@ -756,7 +756,8 @@ down(void)
 void
 lnplus(void)
 {
-	here = col_or_eol(here, 0, MAX_COLS)+(here < pos(ebuf));
+	here = col_or_eol(here, 0, MAX_COLS);
+	here += *ptr(here) == '\n';
 }
 
 /**

test/Curses/textterm/dd4.out
@@ -1,0 +1,26 @@
+<CLEAR>
+<CLEAR>
+<SMSO>a.txt 0B L1                                                                     <RMSO>
+<3;1>
+<SMSO>^D<RMSO>
+<2;1>
+<1;15>
+<SMSO>INS<RMSO>
+<2;1>
+<1;7>
+<SMSO>1<RMSO>
+     <SMSO>*<RMSO>
+<2;1>
+a<1;15>
+<SMSO>   <RMSO>
+<2;2>
+<1;7>
+<SMSO>0<RMSO>
+<2;1>
+<EL>
+<1;1>
+><EL>
+a.txt<1;1>
+<SMSO>a.txt 0B L1                                                                     <RMSO>
+<2;1>
+<24;1>

test/Curses/textterm/dd5.out
@@ -1,0 +1,43 @@
+<CLEAR>
+<CLEAR>
+<SMSO>a.txt 0B L1                                                                     <RMSO>
+<3;1>
+<SMSO>^D<RMSO>
+<2;1>
+<1;15>
+<SMSO>INS<RMSO>
+<2;1>
+<1;7>
+<SMSO>1<RMSO>
+   <SMSO>2<RMSO>
+ <SMSO>*<RMSO>
+<3;1>
+<EL>
+<4;1>
+<SMSO>^D<RMSO>
+<3;1>
+<1;7>
+<SMSO>2<RMSO>
+<3;1>
+1<1;7>
+<SMSO>3<RMSO>
+<3;2>
+2<1;7>
+<SMSO>8<RMSO>
+   <SMSO>3<RMSO>
+   <SMSO>   <RMSO>
+<3;3>
+3<4;1>
+abc<5;1>
+<SMSO>^D<RMSO>
+<4;4>
+<1;7>
+<SMSO>5<RMSO>
+<4;1>
+<EL>
+<1;1>
+><EL>
+a.txt<1;1>
+<SMSO>a.txt 5B L3                                                                     <RMSO>
+<4;1>
+<24;1>

test/Curses/textterm/indent9.out
@@ -1,0 +1,44 @@
+<CLEAR>
+<CLEAR>
+<SMSO>a.txt 0B L1                                                                     <RMSO>
+<3;1>
+<SMSO>^D<RMSO>
+<2;1>
+<1;15>
+<SMSO>INS<RMSO>
+<2;1>
+<1;7>
+<SMSO>1<RMSO>
+   <SMSO>2<RMSO>
+ <SMSO>*<RMSO>
+<3;1>
+<EL>
+<4;1>
+<SMSO>^D<RMSO>
+<3;1>
+<1;7>
+<SMSO>2<RMSO>
+<3;1>
+1<1;7>
+<SMSO>3<RMSO>
+<3;2>
+2<1;7>
+<SMSO>4<RMSO>
+       <SMSO>   <RMSO>
+<3;3>
+3<1;11>
+<SMSO>1<RMSO>
+<2;1>
+<1;7>
+<SMSO>5<RMSO>
+   <SMSO>2<RMSO>
+<3;1>
+        123<1;11>
+<SMSO>1<RMSO>
+<2;1>
+<1;1>
+><EL>
+a.txt<1;1>
+<SMSO>a.txt 5B L1                                                                     <RMSO>
+<2;1>
+<24;1>

test/Makefile
@@ -92,9 +92,9 @@ TESTS:= tests_have_term_data \
 	select0 select1 select2 select3 select4 select5 \
 	mb0 mb1 mb2 mb3 mb4 imb0 imb1 imb2 imb3 imb4 \
 	replace3 replace4 replace5 replace6 replace7 \
-	cc0 cc1 dd0 dd1 dd2 dd3 \
+	cc0 cc1 dd0 dd1 dd2 dd3 dd4 dd5 \
 	indent0 indent1 indent2 indent3 indent4 indent5 \
-	indent6 indent7 indent8 \
+	indent6 indent7 indent8 indent9 \
 	outdent0 outdent1 list0
 
 .PHONY : ${TESTS}
@@ -102,7 +102,7 @@ TESTS:= tests_have_term_data \
 
 tests: $T/terminfo.cdb ${TESTS} clean
 
-tests_skipped: empty2 write1 indent9
+tests_skipped: empty2 write1
 
 # While the terminal definition will probably contain definitions for
 # the number of hardware `lines` and `columns`, we explicitly override
@@ -1202,6 +1202,24 @@ dd3:
 	-${TITLE}
 	cp test/short.txt a.txt
 	printf '4w2d3dW\nQ' | ${PROG} a.txt >a.out
+	${A_CORE}
+	${A_OUT}
+	${A_TXT}
+	-${PASS}
+
+# CB-30
+dd4:
+	-${TITLE}
+	printf 'ia\033ddW\nQ' | ${PROG} a.txt >a.out
+	${A_CORE}
+	${A_OUT}
+	test ! -s a.txt
+	-${PASS}
+
+# CB-30
+dd5:
+	-${TITLE}
+	printf 'i\n123\nabc\033ddW\nQ' | ${PROG} a.txt >a.out
 	${A_CORE}
 	${A_OUT}
 	${A_TXT}